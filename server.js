46760ab2f5667a01951fdcae1d3844e7db640dda
Puentebravo <puentematos@gmail.com>
2021-02-27 10:58:11 -0500

    Removed reference to deprecated file, cleaned up formatting

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
-var express = require("express");
-var session = require("express-session");
-var passport = require("./config/passport");
+const express = require("express");
+const session = require("express-session");
+const passport = require("./config/passport");
 
-var PORT = process.env.PORT || 8080;
-var db = require("./models");
+const PORT = process.env.PORT || 8080;
+const db = require("./models");
 
-var app = express();
+const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
-require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
-db.sequelize.sync().then(function() {
-    app.listen(PORT, function() {
-      console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
-    });
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
   });
+});
